6a43de0b26d937847bb8adac42607671d8278d25
ReaPeRZ RyL <ReaPeR.RyL@outlook.com>
2023-03-29 09:24:06 +0200

    Contact page

---
+++ MvcMovie/Controllers/Contact.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using System.Diagnostics;
+
+namespace MvcMovie.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
